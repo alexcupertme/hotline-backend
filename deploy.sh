b95690801485760b57fe95850b8dde6d578df622
vzlomed <73132261+vzlomed@users.noreply.github.com>
2022-04-07 20:03:50 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,1 @@
-if forever list | grep -q '--uid backend';
-  forever stop backend
-npm run build
-forever start -c "npm run prod" ./ --uid backend
+forever start -c "npm run build" ./ --uid backend
