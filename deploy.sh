516dd1e357ff3dcef18a65ed59e6a1d3076d704d
vzlomed <73132261+vzlomed@users.noreply.github.com>
2022-04-08 01:22:49 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,31 @@
+VALID_ARGS=$(getopt -o abg:d: --long alpha,beta,gamma:,delta: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -r | --redispwd)
+        export REDIS_PWD=$2
+        shift 2
+        ;;
+    -p | --postgrespwd)
+        export POSTGRES_PWD=$2
+        shift 2
+        ;;
+    -u | --postgresusr)
+        export POSTGRES_USR=$2
+        shift 2
+        ;;
+    --) shift; 
+        break 
+        ;;
+  esac
+done
+
+env
+
 pm2 stop backend
 pnpm i
 npm run build
