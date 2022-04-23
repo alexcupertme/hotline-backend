21e1ab4ae8c7da585b68d1a0ff3b5cabdc9f09a8
vzlomed <johndoe@example.com>
2022-04-24 00:40:17 +0300

    Add documentation generator (swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -9,6 +12,18 @@ async function bootstrap() {
         type: VersioningType.URI,
         defaultVersion: '1',
         prefix: 'api/v',
+    })
+
+    const config = new DocumentBuilder()
+        .setTitle('Armapay API')
+        .setDescription('The Armapay API description')
+        .setVersion('1.0')
+        .addTag('common')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+
+    SwaggerModule.setup('api', app, document, {
+        customCss: readFileSync(resolve(__dirname, `../public/swagger/css/theme-flattop.css`), 'utf-8'),
     })
     app.useGlobalPipes(new ValidationPipe())
     await app.listen(5000)
