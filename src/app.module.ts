eb8b7c19303c583ddaa7eea5ea6e2e1288c95833
vzlomed <johndoe@example.com>
2022-05-09 23:24:07 +0300

    Add bull to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common'
 import { UsersModule } from '@models/user/user.module'
+import { Module } from '@nestjs/common'
 import { PostgresDatabaseProviderModule } from 'providers/database/postgres/provider.module'
 import { AuthModule } from './models/auth/auth.module'
 import { RedisDatabaseProviderModule } from './providers/database/redis/provider.module'
+import { BullQueueProviderModule } from './providers/queue/bull/provider.module'
 
 @Module({
-    imports: [PostgresDatabaseProviderModule, RedisDatabaseProviderModule, UsersModule, AuthModule],
+    imports: [
+        PostgresDatabaseProviderModule,
+        RedisDatabaseProviderModule,
+        BullQueueProviderModule,
+        UsersModule,
+        AuthModule,
+    ],
     controllers: [],
 })
 export class AppModule {}
