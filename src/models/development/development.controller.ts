efc4672b1e52e5e36f167f1877eb47e0a3e8d57b
vzlomed <johndoe@example.com>
2022-05-15 23:21:21 +0300

    Add deactivate mail dev endpoint

--- src/models/development/development.controller.ts
+++ src/models/development/development.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { DevelopmentService } from './development.service'
+import { DeactivateUserEmailByEmailRequestDto } from './dto/deactivate-user-email-by-email.dto'
 import { DeleteUserWithMailsByEmailRequestDto } from './dto/delete-user-with-mails-by-email.dto'
 
 @Controller({
@@ -10,7 +11,16 @@ export class DevelopmentController {
     constructor(private readonly developmentService: DevelopmentService) {}
 
     @Post('/delete-user-with-mails-by-email')
-    async requestVerifyMail(@Body() dto: DeleteUserWithMailsByEmailRequestDto): Promise<{ success: boolean }> {
+    async deleteUserWithMailsByEmailRequestDto(
+        @Body() dto: DeleteUserWithMailsByEmailRequestDto
+    ): Promise<{ success: boolean }> {
         return await this.developmentService.deleteUserWithMailsByEmail(dto)
+    }
+
+    @Post('/deactivate-user-email-by-email')
+    async deactivateUserEmailByEmailRequestDto(
+        @Body() dto: DeactivateUserEmailByEmailRequestDto
+    ): Promise<{ success: boolean }> {
+        return await this.developmentService.deactivateUserEmailByEmail(dto)
     }
 }
--- src/models/development/development.service.ts
+++ src/models/development/development.service.ts
@@ -2,6 +2,7 @@ import { MailsRepository } from '@models/mail/mail.repository'
 import { UsersRepository } from '@models/user/user.repository'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { DeactivateUserEmailByEmailRequestDto } from './dto/deactivate-user-email-by-email.dto'
 import { DeleteUserWithMailsByEmailRequestDto } from './dto/delete-user-with-mails-by-email.dto'
 
 @Injectable()
@@ -16,6 +17,11 @@ export class DevelopmentService {
     async deleteUserWithMailsByEmail(dto: DeleteUserWithMailsByEmailRequestDto) {
         await this.mailsRepository.delete({ email: dto.email })
         await this.usersRepository.delete({ email: dto.email })
+        return { success: true }
+    }
+
+    async deactivateUserEmailByEmail(dto: DeactivateUserEmailByEmailRequestDto) {
+        await this.usersRepository.update({ email: dto.email }, { isMailVerified: false })
         return { success: true }
     }
 }
